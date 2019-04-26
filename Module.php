30f32b9db3984867dc8de117cf25a9331bea0975
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:15:19 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * This code is licensed under Afterlogic Software License.
- * For full statements of the license see LICENSE file.
+ * This code is licensed under AGPLv3 license or Afterlogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
  */
 
 namespace Aurora\Modules\CalendarMeetingsPlugin;
 
 /**
+ * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
  *
